48940270a6ad0418b6ac520f0352cccf0f7d3891
ThisisAlam <103932559+ThisisAlam@users.noreply.github.com>
2026-05-30 13:32:39 +0500

    updated the codebase

--- van-life/src/api.js
+++ van-life/src/api.js
@@ -10,15 +10,14 @@ import {
     query,
     where
 } from "firebase/firestore/lite"
-import firebaseConfig from ".env"
-// Your web app's Firebase configuration
+
 const firebaseConfig = {
-  apiKey: "AIzaSyD_LYt8ftpS-B_BxOytNK0EQgItN_8T0j8",
-  authDomain: "vanlife-alam.firebaseapp.com",
-  projectId: "vanlife-alam",
-  storageBucket: "vanlife-alam.firebasestorage.app",
-  messagingSenderId: "225048520324",
-  appId: "1:225048520324:web:82fdbe14c9785a6740c4d1"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
